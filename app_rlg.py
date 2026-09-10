da691371fa4eae888a01fba62921d34457d1a30d
Alex G. Barreto <alexgbarreto@gmail.com>
2026-09-10 12:01:16 -0500

    refactor: actualizar URL_BASE_REMOTE al CDN de Cloudflare Worker

--- app_rlg.py
+++ app_rlg.py
@@ -12,7 +12,7 @@ PATH_JSON = Path("datosrlg.json")
 PATH_LOGO = Path("logocueb.png")
 PATH_MANOS = Path("assets/manos")
 
-URL_BASE_REMOTE = "https://pub-5f40f6c8037e4f68a0750b7c52988517.r2.dev"
+URL_BASE_REMOTE = "https://deixisls.alexgbarreto-3c4.workers.dev"
 
 st.set_page_config(
     page_title="Diccionario LSC | Universidad El Bosque",
