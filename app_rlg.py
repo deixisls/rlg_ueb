1f25fbed4a5bcf6909abb7dbde155cf6e528f135
Alex G. Barreto <alexgbarreto@gmail.com>
2026-09-12 13:03:43 -0500

    feat: actualiza titulo principal

--- app_rlg.py
+++ app_rlg.py
@@ -261,7 +261,7 @@ with st.container():
     
     with col_cred_1:
         st.markdown("""
-       ## **Universidad El Bosque (2026 ©) **
+       ## **Universidad El Bosque (2026 ©)**
 
         **Departamento de Humanidades**
         
